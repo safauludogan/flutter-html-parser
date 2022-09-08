45ddc7694624701a014201111a885b7ee6df68b7
Safa <suludogan95@gmail.com>
2022-09-08 17:36:34 +0300

    08.09.2022

--- lib/core/base/viewmodel/base_view_model.dart
+++ lib/core/base/viewmodel/base_view_model.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobx/mobx.dart';
-//part 'base_view_model.g.dart';
-/*abstract class BaseViewModel {
-  late BuildContext baseContext;
-  void setContext(BuildContext context);
-  void init(TickerProvider? tickerProvider);
-}*/
-
  abstract class BaseViewModel{
   late BuildContext baseContext;
   void setContext(BuildContext context);
